27a9018fcd26cebafd51c49bfd4039ef2cd7ab79
EJ2KDJ <emmanuelle.salga2007@gmail.com>
2025-03-08 19:12:11 +0800

    im tired

--- server.js
+++ server.js
@@ -8,9 +8,7 @@ const port = 4000;
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false,
-    },
+    ssl: { rejectUnauthorized: false }
 });
 
 pool.connect()
