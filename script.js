9d7c6a8093685d01aeef07dced04a9c25e5be45c
Rania <151019884+ranitherat@users.noreply.github.com>
2025-03-22 12:55:49 +0800

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,14 @@ document.addEventListener("DOMContentLoaded", function () {
         navContainer.classList.toggle("show");
     });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Ensure the button exists before adding the event listener
+    let backToTopButton = document.getElementById("backToTop");
+    
+    if (backToTopButton) {
+        backToTopButton.addEventListener("click", function() {
+            document.getElementById("explore").scrollIntoView({ behavior: "smooth" });
+        });
+    }
+});
